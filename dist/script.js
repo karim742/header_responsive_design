e50fd53e3ee784ff4dc91e8c04bd52def0d0c4b9
karim742 <miakarim742@gmail.com>
2023-03-11 09:09:59 +0600

    daynamic bar icon change

--- dist/script.js
+++ dist/script.js
@@ -2,14 +2,14 @@
 
 const toggleBtn = document.getElementById('toggleBtn');
 const navBar = document.querySelector('.navbar');
-const barIcon = document.querySelector('#toggleBtn i');
+const barIcon = document.querySelector('#toggleBtn');
 
 toggleBtn.addEventListener('click',function(){
-  if(!navBar.classList.contains('showNav') && !barIcon.classList.contains('fa-xmark')){
+  if(!navBar.classList.contains('showNav')){
     navBar.classList.add('showNav');
-    barIcon.classList.add('fa-xmark');
+    barIcon.innerHTML = '<i class="fa-solid fa-bars-staggered text-gray-400 text-xl"></i>'
   }else {
     navBar.classList.remove('showNav');
-    barIcon.classList.remove('fa-xmark');
+    barIcon.innerHTML = '<i class="fa-solid fa-bars text-gray-400 text-xl"></i>'
   }
 });
